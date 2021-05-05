040ec15658460c623616f4a497513a00500b6cfb
Nicholas LaChance <nicholasplachance@gmail.com>
2021-05-05 16:44:10 -0500

    Seperate into components, create dynamic data for the expenseItems

---
+++ src/components/ExpenseContainer.js
@@ -0,0 +1,34 @@
+//imports
+
+import './ExpenseContainer.css';
+
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseContainer = (props) => {
+	return (
+		<div className='expenses'>
+			<ExpenseItem
+				title={props.expenses[0].title}
+				amount={props.expenses[0].amount}
+				date={props.expenses[0].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[1].title}
+				amount={props.expenses[1].amount}
+				date={props.expenses[1].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[2].title}
+				amount={props.expenses[2].amount}
+				date={props.expenses[2].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[3].title}
+				amount={props.expenses[3].amount}
+				date={props.expenses[3].date}
+			/>
+		</div>
+	);
+};
+
+export default ExpenseContainer;
